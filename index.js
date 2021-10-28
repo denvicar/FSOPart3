b4c367f8f6afd62f2965269319dc7ff435bfeefd
Ciro Faenza <rkumura3@gmail.com>
2021-10-28 15:19:16 +0200

    exercise3.6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -7,6 +8,7 @@ const generateId = () => {
 }
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
