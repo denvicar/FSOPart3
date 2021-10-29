a0157c21bd81d84e8dd657cfacd4ca6e68c78d64
Ciro Faenza <rkumura3@gmail.com>
2021-10-29 23:21:20 +0200

    exercise3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 
 const app = express()
@@ -9,6 +10,7 @@ const generateId = () => {
 }
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function (req,res) {return JSON.stringify(req.body)})
 
