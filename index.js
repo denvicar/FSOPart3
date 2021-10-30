80d520bc036471f5e41b4dcca293cb1671e136f4
Ciro Faenza <rkumura3@gmail.com>
2021-10-30 22:05:17 +0200

    exercise3.17

--- index.js
+++ index.js
@@ -46,6 +46,18 @@ app.get('/api/persons/:id', (request, response,next) => {
     }).catch(error=>next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const person = {
+        name: request.body.name,
+        number: request.body.number
+    }
+    Person.findByIdAndUpdate(request.params.id,person, {new: true})
+        .then(updatedPerson=>{
+            response.json(updatedPerson)
+        })
+        .catch(error=>next(error))
+})
+
 app.delete('/api/persons/:id', (request, response,next) => {
     Person.findByIdAndRemove(request.params.id).then(result=>{
         response.status(204).end()
