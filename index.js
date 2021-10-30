12fd0d9e0ec7d78124eff974462946238b5d3f8f
Ciro Faenza <rkumura3@gmail.com>
2021-10-30 14:19:03 +0200

    exercise3.13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 
 const app = express()
@@ -22,31 +24,10 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms - 
     skip: function (req,res) {return req.method!=='POST'}
 }))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get("/api/persons",(request,response) => {
-    response.json(persons)
+    Person.find({}).then(result=> {
+        response.json(result)
+    })
 })
 
 app.get('/info', (request,response) => {
@@ -58,15 +39,14 @@ app.get('/info', (request,response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p=>p.id===id)
-
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404)
-        response.end()
-    }
+    Person.findById(request.params.id).then(person=>{
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404)
+            response.end()
+        }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -99,7 +79,7 @@ app.post('/api/persons', (request,response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
